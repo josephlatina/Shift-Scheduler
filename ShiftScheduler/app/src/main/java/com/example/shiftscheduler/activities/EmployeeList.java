56dd13ab600e581e4650c06f93434b82db8ce2c4
Joseph Latina <josephlatina93@gmail.com>
2021-10-10 13:12:59 -0600

    deleted old layouts and export package and added EmployeeList activity and test layout

---
+++ ShiftScheduler/app/src/main/java/com/example/shiftscheduler/activities/EmployeeList.java
@@ -0,0 +1,18 @@
+package com.example.shiftscheduler.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.shiftscheduler.R;
+
+public class EmployeeList extends AppCompatActivity{
+    //references to controls on the layout
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test);
+    }
+}
