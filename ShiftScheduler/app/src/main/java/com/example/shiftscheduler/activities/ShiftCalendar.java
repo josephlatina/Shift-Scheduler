526aec9a71ef0b602e8b32c62c37c3cef958435f
Joseph Latina <josephlatina93@gmail.com>
2021-10-26 15:02:03 -0600

    made ShiftCalendar activity and its layout file

---
+++ ShiftScheduler/app/src/main/java/com/example/shiftscheduler/activities/ShiftCalendar.java
@@ -0,0 +1,16 @@
+package com.example.shiftscheduler.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.shiftscheduler.R;
+
+import android.os.Bundle;
+
+public class ShiftCalendar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.shift_calendar);
+    }
+}
