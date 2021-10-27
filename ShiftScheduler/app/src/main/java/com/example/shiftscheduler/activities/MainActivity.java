7d8159c16ff472ae7085fddc7a58d7ded5681e29
Joseph Latina <josephlatina93@gmail.com>
2021-10-26 20:03:45 -0600

    added shift_calendar layout and started on bottom bar

--- ShiftScheduler/app/src/main/java/com/example/shiftscheduler/activities/MainActivity.java
+++ ShiftScheduler/app/src/main/java/com/example/shiftscheduler/activities/MainActivity.java
@@ -1,4 +1,27 @@
 package com.example.shiftscheduler.activities;
 
-public class MainActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.shiftscheduler.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //setup the bottom navigation menu
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this, R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+    }
 }
