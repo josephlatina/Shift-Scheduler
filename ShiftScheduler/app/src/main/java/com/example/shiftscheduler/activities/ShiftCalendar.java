578685d82d5826fc4189d216b9c9451d169935af
josephlatina <josephlatina93@gmail.com>
2021-10-27 00:20:19 -0600

    created ShiftDay Activity and shift_day layout and started on ShiftCalendar Activity

--- ShiftScheduler/app/src/main/java/com/example/shiftscheduler/activities/ShiftCalendar.java
+++ ShiftScheduler/app/src/main/java/com/example/shiftscheduler/activities/ShiftCalendar.java
@@ -1,16 +1,32 @@
 package com.example.shiftscheduler.activities;
 
+import android.os.Bundle;
+import android.widget.CalendarView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.shiftscheduler.R;
 
-import android.os.Bundle;
-
 public class ShiftCalendar extends AppCompatActivity {
+
+    //references to layout controls
+    CalendarView calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.shift_calendar);
+
+        //Link the layout controls
+        calendar = (CalendarView) findViewById(R.id.calendarView);
+
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String date = year + "-" + month + "-" + dayOfMonth;
+
+            }
+        });
     }
 }
