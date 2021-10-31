f851b8f49aaa093dfbde68927427528406ea10b4
josephlatina <josephlatina93@gmail.com>
2021-10-31 11:55:41 -0600

    made logic for determining when to switch to ShiftWeekDay or ShiftWeekEnd in the ShiftCalendar activity

--- ShiftScheduler/app/src/main/java/com/example/shiftscheduler/activities/ShiftCalendar.java
+++ ShiftScheduler/app/src/main/java/com/example/shiftscheduler/activities/ShiftCalendar.java
@@ -1,13 +1,17 @@
 package com.example.shiftscheduler.activities;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.CalendarView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.shiftscheduler.R;
+
+import java.time.LocalDate;
 
 public class ShiftCalendar extends AppCompatActivity {
 
@@ -23,13 +27,23 @@ public class ShiftCalendar extends AppCompatActivity {
         calendar = (CalendarView) findViewById(R.id.calendarView);
 
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
                 String date = year + "-" + month + "-" + dayOfMonth;
+                LocalDate localDate = LocalDate.parse(date);
+                int dayOfWeek = localDate.getDayOfWeek().getValue();
 
-                Intent myIntent = new Intent(ShiftCalendar.this, ShiftWeekDay.class);
-                myIntent.putExtra("date", date);
-                startActivity(myIntent);
+                //If It's a weekend, switch to the ShiftWeekEnd Activity. Otherwise, switch to ShiftWeekDay
+                if (dayOfWeek == 6 || dayOfWeek == 7) {
+                    Intent myIntent = new Intent(ShiftCalendar.this, ShiftWeekEnd.class);
+                    myIntent.putExtra("date", date);
+                    startActivity(myIntent);
+                } else {
+                    Intent myIntent = new Intent(ShiftCalendar.this, ShiftWeekDay.class);
+                    myIntent.putExtra("date", date);
+                    startActivity(myIntent);
+                }
             }
         });
     }
