b0f60dd2c87742532ad85ea5716c7e9dfe72adaa
Joseph Latina <josephlatina93@gmail.com>
2021-11-15 14:48:41 -0700

    created employee time off activity and layout

---
+++ ShiftScheduler/app/src/main/java/com/example/shiftscheduler/activities/EmployeeTimeOff.java
@@ -0,0 +1,14 @@
+package com.example.shiftscheduler.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmployeeTimeOff extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.employee_timeoff);
+    }
+}
