c3394c41298744d33d110556f1b7011ca25c7723
josephlatina <josephlatina93@gmail.com>
2021-11-19 09:26:16 -0700

    formatted dob fields in add and edit employee form to show datepicker dialogs

--- ShiftScheduler/app/src/main/java/com/example/shiftscheduler/activities/EmployeeTimeOff.java
+++ ShiftScheduler/app/src/main/java/com/example/shiftscheduler/activities/EmployeeTimeOff.java
@@ -9,7 +9,6 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
@@ -134,8 +133,6 @@ public class EmployeeTimeOff extends AppCompatActivity {
 
                 updateTimeoffList();
                 buildRecyclerView();
-
-                Toast.makeText(EmployeeTimeOff.this, String.valueOf(adapter.getItemCount()), Toast.LENGTH_SHORT).show();
             }
         });
 
